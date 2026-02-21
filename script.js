aa2f575157d1b694d3da46b23a53b1292a0ea8a6
FZoltan40 <farkasz@kkszki.hu>
2026-02-21 11:02:04 +0100

    LoadComponent is ready to use.

---
+++ script.js
@@ -0,0 +1,38 @@
+const navLinks = document.querySelectorAll('.nav-link');
+const contentDiv = document.getElementById('content');
+const navbarCollapse = document.getElementById('navbarNav');
+
+async function loadComponent(name) {
+    try {
+        //HML betöltése
+        const response = await fetch(`components/${name}.html`);
+        if (!response.ok) throw new Error('Nem sikerült a komponenst betölteni.');
+
+        const html = await response.text();
+        contentDiv.innerHTML = html;
+
+
+
+    }
+    catch {
+
+    }
+}
+
+//ALapértelmezett betöltés
+loadComponent('home');
+
+navLinks.forEach(link => {
+    link.addEventListener('click', e => {
+        e.preventDefault();
+
+        //Aktív link
+        navLinks.forEach(l => l.classList.remove('active'));
+        link.classList.add('active');
+
+        //Komponens betöltése
+        loadComponent(link.dataset.target);
+
+    })
+})
+
